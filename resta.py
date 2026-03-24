e1255d7bb002d8de89f119ed9448f1e1689a1e57
ERSP17 <renesdaperez91@gmail.com>
2026-03-24 12:30:29 -0700

    Agrego mi funcion

---
+++ resta.py
@@ -0,0 +1,8 @@
+def resta():
+    a = int(input("Ingresa el primer número: "))
+    b = int(input("Ingresa el segundo número: "))
+    print("La suma es:", a * b)
+
+
+if __name__ == "__main__":
+    resta()
