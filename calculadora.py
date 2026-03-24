72aeb8bd776cee372468e97f7dfdba699b26c2b7
guillermo <al07038162@tecmilenio.mx>
2026-03-24 14:58:38 -0700

    Registro inicial de calculadora en main

---
+++ calculadora.py
@@ -0,0 +1,9 @@
+def suma():
+    a = int(input("Ingresa el primer número: "))
+    b = int(input("Ingresa el segundo número: "))
+    print("La suma es:", a + b)
+
+
+if __name__ == "__main__":
+    suma()
+    from resta 
