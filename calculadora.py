98914993346d309d1a08a331ebb3899aca32255e
guillermo <al07038162@tecmilenio.mx>
2026-03-24 15:42:42 -0700

    Todo integrado FALTA DIVISIÓN

--- calculadora.py
+++ calculadora.py
@@ -1,19 +1,19 @@
 def sumar():
-    a = int(input("Ingresa el primer número: "))
-    b = int(input("Ingresa el segundo número: "))
+    a = int(input("Ingresa un número: "))
+    b = int(input("Ingresa otro número: "))
     print("La suma es:", a + b)
-
-
-if __name__ == "__main__":
-    sumar()
 
 def multiplicar():
     a = int(input("Ingresa un número: "))
     b = int(input("Ingresa otro número: "))
     print("La multiplicación es:", a * b)
 
+def resta():
+    a = int(input("Ingresa un número: "))
+    b = int(input("Ingresa otro número: "))
+    print("La resta es:", a - b)
 
 if __name__ == "__main__":
+    sumar()
     multiplicar()
-
-
+    resta()rm resta.py
