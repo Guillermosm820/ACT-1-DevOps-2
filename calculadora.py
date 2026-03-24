74cc16f4df124998b622a637322031e8532d8e49
guillermo <al07038162@tecmilenio.mx>
2026-03-23 19:09:13 -0700

    Multiplicación calculadora entry

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,8 @@
-a = int(input("Ingresa un número: "))
-b = int(input("Ingresa otro número: "))
+def multiplicar():
+    a = int(input("Ingresa un número: "))
+    b = int(input("Ingresa otro número: "))
+    print("La multiplicación es:", a * b)
 
-print("La multiplicación es:", a * b)
+
+if __name__ == "__main__":
+    multiplicar()
