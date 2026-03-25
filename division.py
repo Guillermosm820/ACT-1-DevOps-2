210a1ab8691ba7f287e35f9ef40b6f0864ec18ab
ERSP17 <renesdaperez91@gmail.com>
2026-03-24 23:08:35 -0700

    divs

--- division.py
+++ division.py
@@ -1,7 +1,7 @@
 def division():
     a = int(input("Ingresa el primer número: "))
     b = int(input("Ingresa el segundo número: "))
-    print("La division es:", a / b)
+    print("La division es :", a / b)
 
 
 if __name__ == "__main__":
