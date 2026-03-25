f696915e43aa6726de2b5ca5bfc7abfaed8a71a1
ReneTec17 <al07008920@tecmilenio.mx>
2026-03-25 09:31:08 -0700

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -13,7 +13,13 @@ def resta():
     b = int(input("Ingresa otro número: "))
     print("La resta es:", a - b)
 
+def division ():
+    a = int(input("Ingresa el primer número: "))
+    b = int(input("Ingresa el segundo número: "))
+    print("La division es :", a / b)
+
 if __name__ == "__main__":
     sumar()
     multiplicar()
-    resta()rm resta.py
+    resta()
+    division()
